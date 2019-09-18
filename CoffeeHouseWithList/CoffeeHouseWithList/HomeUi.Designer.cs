0a20ccd871e31046979732b5de1a5fda5341fc71
prantoshon <prantoshon14@gmail.com>
2019-09-18 23:17:00 +0600

    upload all file

---
+++ CoffeeHouseWithList/CoffeeHouseWithList/HomeUi.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace CoffeeHouseWithList
+{
+    partial class HomeUi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.showResultRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.contractNumberTextBox = new System.Windows.Forms.TextBox();
+            this.addressTextBox = new System.Windows.Forms.TextBox();
+            this.quantityTextBox = new System.Windows.Forms.TextBox();
+            this.orderComboBox = new System.Windows.Forms.ComboBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.showButton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // showResultRichTextBox
+            // 
+            this.showResultRichTextBox.Location = new System.Drawing.Point(608, 47);
+            this.showResultRichTextBox.Name = "showResultRichTextBox";
+            this.showResultRichTextBox.Size = new System.Drawing.Size(263, 411);
+            this.showResultRichTextBox.TabIndex = 1;
+            this.showResultRichTextBox.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(49, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Customer Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(49, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Customer No";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(49, 156);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Address";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(49, 223);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Order";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(49, 274);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(76, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Quantity";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(239, 66);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(132, 22);
+            this.nameTextBox.TabIndex = 5;
+            // 
+            // contractNumberTextBox
+            // 
+            this.contractNumberTextBox.Location = new System.Drawing.Point(239, 113);
+            this.contractNumberTextBox.Name = "contractNumberTextBox";
+            this.contractNumberTextBox.Size = new System.Drawing.Size(132, 22);
+            this.contractNumberTextBox.TabIndex = 6;
+            // 
+            // addressTextBox
+            // 
+            this.addressTextBox.Location = new System.Drawing.Point(239, 158);
+            this.addressTextBox.Name = "addressTextBox";
+            this.addressTextBox.Size = new System.Drawing.Size(132, 22);
+            this.addressTextBox.TabIndex = 7;
+            // 
+            // quantityTextBox
+            // 
+            this.quantityTextBox.Location = new System.Drawing.Point(239, 274);
+            this.quantityTextBox.Name = "quantityTextBox";
+            this.quantityTextBox.Size = new System.Drawing.Size(132, 22);
+            this.quantityTextBox.TabIndex = 8;
+            // 
+            // orderComboBox
+            // 
+            this.orderComboBox.FormattingEnabled = true;
+            this.orderComboBox.Items.AddRange(new object[] {
+            "Black-120",
+            "Cold-100",
+            "Hot-90",
+            "Regular-80"});
+            this.orderComboBox.Location = new System.Drawing.Point(239, 221);
+            this.orderComboBox.Name = "orderComboBox";
+            this.orderComboBox.Size = new System.Drawing.Size(132, 24);
+            this.orderComboBox.TabIndex = 9;
+            // 
+            // addButton
+            // 
+            this.addButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.addButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addButton.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.addButton.Location = new System.Drawing.Point(138, 337);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(82, 28);
+            this.addButton.TabIndex = 10;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = false;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.showButton);
+            this.groupBox1.Controls.Add(this.addButton);
+            this.groupBox1.Controls.Add(this.orderComboBox);
+            this.groupBox1.Controls.Add(this.quantityTextBox);
+            this.groupBox1.Controls.Add(this.addressTextBox);
+            this.groupBox1.Controls.Add(this.contractNumberTextBox);
+            this.groupBox1.Controls.Add(this.nameTextBox);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(42, 47);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(465, 411);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Customer Information";
+            // 
+            // showButton
+            // 
+            this.showButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.showButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.showButton.Location = new System.Drawing.Point(252, 337);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(82, 28);
+            this.showButton.TabIndex = 11;
+            this.showButton.Text = "Show";
+            this.showButton.UseVisualStyleBackColor = false;
+            this.showButton.Click += new System.EventHandler(this.showButton_Click);
+            // 
+            // HomeUi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(906, 492);
+            this.Controls.Add(this.showResultRichTextBox);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "HomeUi";
+            this.Text = "Coffee Shop";
+            this.Load += new System.EventHandler(this.HomeUi_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox showResultRichTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox contractNumberTextBox;
+        private System.Windows.Forms.TextBox addressTextBox;
+        private System.Windows.Forms.TextBox quantityTextBox;
+        private System.Windows.Forms.ComboBox orderComboBox;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button showButton;
+
+    }
+}
+
---
+++ CoffeeHouseWithList/CoffeeHouseWithList/HomeUi.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CoffeeHouseWithList
+{
+    public partial class HomeUi : Form
+    {
+        List<string> customerNames = new List<string> { };
+        List<string> customerNumbers = new List<string> { };
+        List<string> customerAddresses = new List<string> { };
+        List<string> orders = new List<string> { };
+        List<int> quantitys= new List<int> { };
+        List<int> bills = new List<int> { };
+        
+
+        int totalBillCalculator;
+        int count = 0;
+
+
+        public HomeUi()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void HomeUi_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void CustomerName() 
+        {
+            customerNames.Add(nameTextBox.Text);
+        }
+
+        private void ContractNumber() 
+        {
+           
+            if (!customerNumbers.Contains(contractNumberTextBox.Text))
+            {
+              
+                customerNumbers.Add(contractNumberTextBox.Text);
+                
+            }
+            else 
+            {
+                MessageBox.Show("Contract number is already use");
+
+                return;
+            }
+           
+        }
+
+        private void CustomerAddress() 
+        {
+            customerAddresses.Add(addressTextBox.Text);
+        }
+
+        private void Order() 
+        {
+
+            if (!String.IsNullOrEmpty(orderComboBox.Text))
+            {
+                orders.Add(orderComboBox.Text);
+            }
+            else 
+            {
+                MessageBox.Show("Order Is Empty");
+                return;
+            }
+        }
+
+        private void Quantity() 
+        {
+            if (quantityTextBox.Text !=null)
+            {
+               
+                quantitys.Add(Convert.ToInt32(quantityTextBox.Text));
+            }
+            else
+            {
+                MessageBox.Show("Quantity is null");
+                
+            }
+        }
+
+        private void BillCalculate()
+        {
+            int totalQuantity;
+            if (orderComboBox.Text == "Black-120") 
+            {
+               totalQuantity = Convert.ToInt32(quantityTextBox.Text);
+               totalBillCalculator =  totalQuantity * 120;
+             
+
+            }
+            else if (orderComboBox.Text == "Cold-100")
+            {
+                totalQuantity = Convert.ToInt32(quantityTextBox.Text);
+                totalBillCalculator = totalQuantity * 100;
+         
+            }
+
+            else if (orderComboBox.Text == "Hot-90")
+            {
+                totalQuantity = Convert.ToInt32(quantityTextBox.Text);
+                totalBillCalculator = totalQuantity * 90;
+              
+            }
+            else if (orderComboBox.Text == "Regular-80")
+            {
+                totalQuantity = Convert.ToInt32(quantityTextBox.Text);
+                totalBillCalculator = totalQuantity * 80;
+             
+            }
+            bills.Add(totalBillCalculator);
+        }
+
+     
+
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            showResultRichTextBox.Text = string.Empty;
+            CustomerName();
+            ContractNumber();
+            CustomerAddress();
+            Order();
+            BillCalculate();
+            
+
+            showResultRichTextBox.AppendText("Name : " +nameTextBox.Text+" \n\n"+ "Number : " + contractNumberTextBox.Text +"\n\n"+ "Address : "+ addressTextBox.Text +"\n\n"+"Order : "+ orderComboBox.Text+" \n\n"+ "Quanityt : "+ quantityTextBox.Text +" \n\n"+"Total Bill :  "+ totalBillCalculator);
+            nameTextBox.Text = string.Empty;
+            contractNumberTextBox.Text = string.Empty;
+            addressTextBox.Text = string.Empty;
+            orderComboBox.Text = string.Empty;
+            quantityTextBox.Text = string.Empty;
+
+            count++;
+
+            
+        }
+
+        private void ShowAllRecords()
+        {
+            showResultRichTextBox.Clear();
+            for (int i = 0; i < customerNames.Count; i++)
+            {
+
+                showResultRichTextBox.Text += "Name : " + customerNames[i] + "\n" + "Contract Number : " + customerNumbers[i] + "\n" + "Customer Address : " + customerAddresses[i] + "\n" + "Total Bill : " + bills[i] + "\n\n";
+            }
+
+      
+
+           
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            ShowAllRecords();
+        }
+    }
+}
